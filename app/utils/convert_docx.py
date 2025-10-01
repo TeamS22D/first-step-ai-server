3993ed5f070ce8ed42270eda567c4761d21da5c3
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-10-01 21:44:50 +0900

    feat(util): docx에서 text, table, styles 추출 및 테스트 코드

--- app/utils/convert_docx.py
+++ app/utils/convert_docx.py
@@ -8,3 +8,33 @@ def convert_docx(file_path: str):
         text_content.append(para.text)
 
     return '\n'.join(text_content)
+
+
+def convert_docx_table(file_path: str):
+    document = Document(file_path)
+    table_content = []
+
+    for table in document.tables:
+        rows = []
+        for row in table.rows:
+            cells = [cell.text.strip() for cell in row.cells]
+            rows.append(cells)
+
+        table_content.append(rows)
+
+    return table_content
+
+def parse_styles(file_path: str):
+    document = Document(file_path)
+
+    styles_content = []
+
+    for style in document.styles:
+
+        styles_content.append(style.name)
+
+    for para in document.paragraphs:
+        styles_content.append(para.style)
+
+
+    return styles_content
--- tests/test_utils/test_convert_docx.py
+++ tests/test_utils/test_convert_docx.py
@@ -1,5 +1,6 @@
 import pytest
-from app.utils.convert_docx import convert_docx
+from app.utils.convert_docx import *
+from pprint import pprint
 import os
 
 
@@ -10,3 +11,24 @@ def test_convert_docx():
     abs_path = os.path.abspath(os.path.dirname(__file__)) + "/" + file_path
 
     print(convert_docx(file_path=abs_path))
+
+def test_convert_docx_table():
+    print("\n\n#### test convert docx ####\n\n")
+
+    file_path = "./test_docx_2.docx"
+    abs_path = os.path.abspath(os.path.dirname(__file__)) + "/" + file_path
+
+    pprint(convert_docx_table(file_path=abs_path))
+
+def test_parse_list():
+    print("\n\n#### test parse list ####\n\n")
+
+    file_path = "./test_docx_1.docx"
+    abs_path = os.path.abspath(os.path.dirname(__file__)) + "/" + file_path
+
+    pprint(parse_styles(file_path=abs_path))
+
+    file_path = "./test_docx_2.docx"
+    abs_path = os.path.abspath(os.path.dirname(__file__)) + "/" + file_path
+
+    pprint(parse_styles(file_path=abs_path))
