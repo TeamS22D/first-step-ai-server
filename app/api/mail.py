0f92a77f8e195ba262ecf048f64697d2d7ad8c52
ys0v9 <sua9097@gmail.com>
2025-12-08 00:22:01 +0900

    delete mail_ex_3

--- app/api/mail.py
+++ app/api/mail.py
@@ -54,7 +54,6 @@ def evaluate_mission_email(mission_id: str, user_email: str):
     mission_map = {
         "mail_ex_1": "mail_1",
         "mail_ex_2": "mail_2",
-        "mail_ex_3": "mail_3"
     }
 
     actual_mission_id = mission_map.get(mission_id)
